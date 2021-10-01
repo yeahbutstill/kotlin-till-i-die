e647c20ba6df80227ff3f3b8f9ec04030a5b5b1b
yeahbutstill <mayadani28@gmail.com>
2021-10-01 18:27:37 +0700

    constructor bukan kontraktor apa lagi kontrakan iyaiyaiya

--- src/main/kotlin/app/Yeah.kt
+++ src/main/kotlin/app/Yeah.kt
@@ -3,6 +3,12 @@ package app
 import oop.`object`.`class`.Car
 import oop.`object`.`class`.Person
 
+/**
+ * Main
+ * Constructor adalah function yang pertama kali dieksekusi ketika object itu
+ * dibuat
+ *
+ */
 fun main() {
     val dani = Person("Dani",
         "Setiawan","SKOM") // Parameter Constructor
