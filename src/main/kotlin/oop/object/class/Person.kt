d83c74e6460f35e9a4bf1c18846ee13a9f9455a8
yeahbutstill <mayadani28@gmail.com>
2021-10-01 18:41:35 +0700

    jangan terlarut atau terlalu berat naro perintah di Initializer Block

--- src/main/kotlin/oop/object/class/Person.kt
+++ src/main/kotlin/oop/object/class/Person.kt
@@ -3,6 +3,10 @@ package oop.`object`.`class`
 class Person(firstNameParam: String,
              middleNameParam: String?, lastNameParam: String) { // Constructor
 
+    init {
+        println("Manusia buatan bernama : $firstNameParam dibuat") // Initializer ini akan selalu dieksekusi pada objek dibuat
+    }
+
     var firstName: String = firstNameParam
     var middleName: String? = middleNameParam
     var lastName: String = lastNameParam
