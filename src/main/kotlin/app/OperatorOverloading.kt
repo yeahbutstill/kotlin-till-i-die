959166e1a017c081b6e77d0182a8b7ecf4b82b2f
yeahbutstill <mayadani28@gmail.com>
2021-10-07 00:27:35 +0700

    finis Operator Overloading

--- src/main/kotlin/app/OperatorOverloading.kt
+++ src/main/kotlin/app/OperatorOverloading.kt
@@ -4,5 +4,7 @@ import oop.`object`.`class`.Fruit
 
 fun main() {
     val fruit = Fruit(100) + Fruit(100)
+    val fruit1 = Fruit(500) - Fruit(400)
     println(fruit)
+    println(fruit1)
 }
--- src/main/kotlin/oop/object/class/Fruit.kt
+++ src/main/kotlin/oop/object/class/Fruit.kt
@@ -5,4 +5,8 @@ data class Fruit(val total: Int) {
     operator fun plus(fruit: Fruit): Fruit {
         return Fruit(total + fruit.total)
     }
+
+    operator fun minus(fruit: Fruit): Fruit {
+        return Fruit(total - fruit.total)
+    }
 }
