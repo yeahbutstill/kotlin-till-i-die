fe7158e6b152f4d7953cdc34d5cb2d83549c42d4
yeahbutstill <mayadani28@gmail.com>
2021-10-13 12:52:42 +0700

    finis association

--- src/main/kotlin/collection/Association.kt
+++ src/main/kotlin/collection/Association.kt
@@ -1,2 +1,14 @@
 package collection
 
+fun main() {
+    val list: List<String> = listOf("Dani", "Setiawan", "Gandes")
+
+    val map: Map<String, Int> = list.associate { Pair(it, it.length) }
+    println(map)
+
+    val map2: Map<String, Int> = list.associateWith { it.length }
+    println(map2)
+
+    val map3: Map<Int, String> = list.associateBy { it.length }
+    println(map3)
+}
