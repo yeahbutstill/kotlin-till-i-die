38035c4cb8e9448196725e0b65babbf92c7d6c62
yeahbutstill <mayadani28@gmail.com>
2021-10-14 06:18:04 +0700

    finis chucked memotong collection

---
+++ src/main/kotlin/collection/Chunked.kt
@@ -0,0 +1,17 @@
+package collection
+
+fun main() {
+    val range = (1..100).toList()
+    val list1 = range.chunked(10)
+    println(list1.size)
+    println(list1)
+    val list2 = range.chunked(10) {
+        list: List<Int> ->
+        var total = 0
+        for (number in list) {
+            total += number
+        }
+        total
+    }
+    println(list2)
+}
